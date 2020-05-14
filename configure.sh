d26e1d9e11370e97a7d5c1ca2d6bebba44ad9a44
polatengin <polatengin@outlook.com>
2020-05-14 23:05:56 +0300

    make .bash_profile file to load files in this repo

--- configure.sh
+++ configure.sh
@@ -6,3 +6,10 @@ mkdir .dotfiles
 
 git clone https://github.com/polatengin/dotfiles.git
 
+echo ". ~/.dotfiles/.bash_aliases" >> ~/.bash_profile
+echo ". ~/.dotfiles/.bash_functions" >> ~/.bash_profile
+echo ". ~/.dotfiles/.bash_profile" >> ~/.bash_profile
+echo ". ~/.dotfiles/.bash_prompt" >> ~/.bash_profile
+
+source ~/.bash_profile
+
