4d8ec0f1a2d076cf5fa076f78c3a62be5d7ae6f3
polatengin <polatengin@outlook.com>
2020-05-15 08:00:13 +0300

    sudos removed

--- configure.sh
+++ configure.sh
@@ -23,26 +23,26 @@ echo "cd /p" >> ~/.bash_profile
 
 # update and upgrade current packages
 
-sudo apt-get update
-sudo apt-get -y upgrade
+apt-get update
+apt-get -y upgrade
 
 # install go
 
 cd /tmp
 wget https://dl.google.com/go/go1.14.2.darwin-amd64.tar.gz
 
-sudo tar -xvf go1.14.2.darwin-amd64.tar.gz
-sudo mv go /usr/local
+tar -xvf go1.14.2.darwin-amd64.tar.gz
+mv go /usr/local
 
 echo "export PATH=\"/usr/local/go/bin\":\${PATH}" >> ~/.bash_profile
 
 # install dotnet
 
 wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
 
-sudo add-apt-repository universe
-sudo apt-get update
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-3.1
+add-apt-repository universe
+apt-get update
+apt-get install -y apt-transport-https
+apt-get update
+apt-get install -y dotnet-sdk-3.1
