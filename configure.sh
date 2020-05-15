ecba62cb93653fb2ef413ab2ae0002ea466f0698
polatengin <polatengin@outlook.com>
2020-05-15 14:45:33 +0300

    azure cli installed

--- configure.sh
+++ configure.sh
@@ -43,6 +43,17 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-3.1
 
+# install azure cli
+echo $'\n########\ninstall azure cli\n'
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install azure-cli
+
 # install kubectl
 echo $'\n########\ninstall kubectl\n'
 
