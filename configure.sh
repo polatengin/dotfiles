869d844513bdc24d1ea25232a7a45015abb8f4e9
polatengin <polatengin@outlook.com>
2020-05-15 11:25:13 +0300

    sourced files removed from configuration script

--- configure.sh
+++ configure.sh
@@ -10,13 +10,6 @@ cd ~/
 
 git clone https://github.com/polatengin/dotfiles.git ~/.dotfiles
 
-echo ". ~/.dotfiles/.bash_aliases" >> ~/.bash_profile
-echo ". ~/.dotfiles/.bash_functions" >> ~/.bash_profile
-echo ". ~/.dotfiles/.bash_profile" >> ~/.bash_profile
-echo ". ~/.dotfiles/.bash_prompt" >> ~/.bash_profile
-
-source ~/.bash_profile
-
 # create projects folder
 
 mkdir -p /p
