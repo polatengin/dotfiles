e4081bff83082b065a9c0e79a1b1b3811f6575ce
polatengin <polatengin@outlook.com>
2020-05-15 08:23:48 +0300

    kubectl added to install list

--- configure.sh
+++ configure.sh
@@ -39,3 +39,9 @@ apt-get update
 apt-get install -y apt-transport-https
 apt-get update
 apt-get install -y dotnet-sdk-3.1
+
+# install kubectl
+
+cd /usr/local/bin
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
