1355cd30572ba444ead64c39ad41cf448f7f500e
polatengin <polatengin@outlook.com>
2020-05-15 08:23:35 +0300

    dotnet installer updated to ubuntu20.04

--- configure.sh
+++ configure.sh
@@ -32,10 +32,9 @@ apt install golang
 
 # install dotnet
 
-wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 
-add-apt-repository universe
 apt-get update
 apt-get install -y apt-transport-https
 apt-get update
