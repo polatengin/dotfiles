e489c11eae3b97bf34481aba5a5d364160e487e5
polatengin <enpolat@microsoft.com>
2020-11-10 19:42:15 +0000

    dotnet installer upgraded to .net 5

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y apt-transport-https
 sudo apt-get update
-sudo apt-get install -y dotnet-sdk-3.1
+sudo apt-get install -y dotnet-sdk-5.0
 
 # install azure function core tools
 echo $'\n########\ninstall azure function core tools\n'
