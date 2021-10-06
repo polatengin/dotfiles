8200f6d8d7ab3d08259091bb0eceeb9077ac10f2
Engin Polat <enpolat@microsoft.com>
2021-10-05 23:37:13 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -184,6 +184,15 @@ echo $'\n########\ninstall midnight commander\n'
 
 sudo apt-get install -y mc
 
+# install github cli
+
+echo $'\n########\ninstall github cli\n'
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 # make .bash_profile persistent
 
 echo "source ~/.dotfiles/.bash_profile" >> ~/.bashrc
